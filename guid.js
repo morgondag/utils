53ad9392816f7a1936b68b091b5455d33adc713e
Kim Gunnarsson <kim@morgondag.nu>
2015-06-17 14:29:28 +0200

    adding some stuff

--- guid.js
+++ guid.js
@@ -1,13 +1,11 @@
+var generated = [];
 
-
-
-/*
 var lut = [];
 for (var i = 0; i < 256; i++) {
 	lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
 }
 
-function guid() {
+exports.generate = function(){
 	var d0 = (twister.random()) * 0xffffffff | 0;
 	var d1 = (twister.random()) * 0xffffffff | 0;
 	var d2 = (twister.random()) * 0xffffffff | 0;
@@ -17,4 +15,14 @@ function guid() {
 		lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
 		lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
 }
-*/
+
+exports.find = function(guid){
+	return generated.indexOf(guid) >= 0;
+}
+
+exports.remove = function(guid){
+	var index = generated.indexOf()
+	if(index > -1){
+		generated.splice(index,1);
+	}
+}
